8b9ee7dab882852e4ce6e745aafe4db410039083
Nick Vander Heyden <vandy.nick@gmail.com>
2015-10-29 20:05:11 -0500

    Initial speedometer commit. Contains basic speedo and an ArrayList tracker.

---
+++ src/app/src/test/java/com/team/runningapp/GPSSpeedo.java
@@ -0,0 +1,47 @@
+package com.team.runningapp;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class GPSSpeedo implements LocationListener
+{
+    @Override
+    public void onLocationChanged(Location location) {
+        //if no location change data is sent
+        if(location == null) {
+            txt.setText("-.- m/s");
+        }
+        else { //else location data was given
+            //Get the user's speed from location object
+            double nCurrentSpeed = location.getSpeed();
+
+            //Add the user's current speed to the list
+            speedReports.add(nCurrentSpeed);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
