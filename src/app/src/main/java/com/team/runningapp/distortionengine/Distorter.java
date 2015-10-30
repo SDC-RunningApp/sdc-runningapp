ef31b0ecfdc4ffbb2729a293ec5f8a77f8c41dd0
Sunny Shah <shahsunny712@gmail.com>
2015-10-29 19:42:51 -0500

    Added constructor and sound playing methods for the distortion class.
    [Major modifications expected in the future]

--- src/app/src/main/java/com/team/runningapp/distortionengine/Distorter.java
+++ src/app/src/main/java/com/team/runningapp/distortionengine/Distorter.java
@@ -1,4 +1,21 @@
 package com.team.runningapp.distortionengine;
 
+import android.media.MediaPlayer;
+import android.content.Context;
+
 public class Distorter {
+
+    private MediaPlayer mediaPlayer;
+    private Context applicationContext;
+
+    public Distorter(Context _appContext) {
+        applicationContext = _appContext;
+        mediaPlayer = null;
+    }
+
+    public void playSound() {
+        mediaPlayer = MediaPlayer.create(applicationContext, /*sound file*/);
+        mediaPlayer.start();
+    }
+
 }
